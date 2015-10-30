9c622a5d09c0d6f2474a6a7f910f97de8990f84a
Albert Kwon <kwonal@mit.edu>
2015-10-30 17:09:50 -0400

    some skeleton code for client

--- client.go
+++ client.go
@@ -1,31 +1,40 @@
 package main
 
 import (
-	"encoding/json"
 	"crypto"
 	sign "crypto/ecdsa"
 	"crypto/elliptic"
 	"crypto/rand"
+	"encoding/json"
 	"github.com/kwonalbert/spacecoin/block"
 	"github.com/kwonalbert/spacecoin/pos"
 	"github.com/kwonalbert/spacecoin/util"
 	"golang.org/x/crypto/sha3"
 	"math"
 	"math/big"
+	//"net"
+	"net/rpc"
 	"time"
 )
 
 type Client struct {
-	sk              crypto.Signer     // signing secretkey
-	pk              crypto.PublicKey  // signing pubkey
-	t               time.Duration     // how soon we add a block
-	dist            int               // how far to look back for challenge
+	//client and system params
+	sk   crypto.Signer    // signing secretkey
+	pk   crypto.PublicKey // signing pubkey
+	t    time.Duration    // how soon we add a block
+	dist int              // how far to look back for challenge
+
+	//round
+	sols chan *block.Block // others' blocks
 
 	//pos params
-	size            int
-	prover          *pos.Prover
-	verifier        *pos.Verifier
-	commit          pos.Commitment
+	size     int
+	prover   *pos.Prover
+	verifier *pos.Verifier
+	commit   pos.Commitment
+
+	chain   *block.BlockChain
+	clients []*rpc.Client
 }
 
 func NewClient(t time.Duration, dist int, size, beta int, graph string) *Client {
@@ -40,14 +49,16 @@ func NewClient(t time.Duration, dist int, size, beta int, graph string) *Client 
 	}
 
 	prover := pos.NewProver(pkBytes, size, graph)
-	commit := prover.InitGraph()
+	commit := prover.Init()
 	verifier := pos.NewVerifier(pkBytes, size, beta, commit.Commit)
 
 	c := Client{
-		sk:       sk,
-		pk:       pk,
-		t:        t,
-		dist:     dist,
+		sk:   sk,
+		pk:   pk,
+		t:    t,
+		dist: dist,
+
+		sols: make(chan *block.Block, 100), // nomially say 100 answers per round..
 
 		size:     size,
 		prover:   prover,
@@ -61,7 +72,7 @@ func (c *Client) Sign(msg []byte) ([]byte, error) {
 	return c.sk.Sign(rand.Reader, msg, crypto.SHA3_256)
 }
 
-func (c *Client) Mine(challenge []byte) *block.PoS{
+func (c *Client) Mine(challenge []byte) *block.PoS {
 	nodes := c.verifier.SelectChallenges(challenge)
 	hashes, proofs := c.prover.ProveSpace(nodes)
 	a := block.Answer{
@@ -70,10 +81,10 @@ func (c *Client) Mine(challenge []byte) *block.PoS{
 		Proofs: proofs,
 	}
 	p := block.PoS{
-		Commit: c.commit,
+		Commit:    c.commit,
 		Challenge: challenge,
-		Answer: a,
-		Quality: c.Quality(challenge, a),
+		Answer:    a,
+		Quality:   c.Quality(challenge, a),
 	}
 
 	return &p
@@ -81,7 +92,7 @@ func (c *Client) Mine(challenge []byte) *block.PoS{
 
 // Compute quality of the answer. Also builds a verifier
 // return: quality in float64
-func (c Client) Quality(challenge []byte, a block.Answer) float64 {
+func (c *Client) Quality(challenge []byte, a block.Answer) float64 {
 	nodes := c.verifier.SelectChallenges(challenge)
 	if !c.verifier.VerifySpace(nodes, a.Hashes, a.Proofs) {
 		return -1
@@ -89,15 +100,70 @@ func (c Client) Quality(challenge []byte, a block.Answer) float64 {
 
 	all := util.Concat(a.Hashes)
 	for i := range a.Proofs {
-		all = append(all, util.Concat(a.Proofs[i]) ...)
+		all = append(all, util.Concat(a.Proofs[i])...)
 	}
 	answerHash := sha3.Sum256(all)
 	x := new(big.Float).SetInt(new(big.Int).SetBytes(answerHash[:]))
 	num, _ := util.Root(x, a.Size).Float64()
-	den := math.Exp2(float64(1 << 8)/float64(a.Size))
-	return num/den
+	den := math.Exp2(float64(1<<8) / float64(a.Size))
+	return num / den
 }
 
+// Generate challenge from older blocks
+// return: challenge for next block []byte
+func (c *Client) GenerateChallenge() []byte {
+	var b *block.Block
+	var err error
+	if c.chain.LastBlock < c.dist {
+		b, err = c.chain.Read(c.chain.LastBlock)
+	} else {
+		b, err = c.chain.Read(c.chain.LastBlock - (c.dist - 1))
+	}
+	if err != nil {
+		panic(err)
+	}
+	bin, err := b.MarshalBinary()
+	if err != nil {
+		panic(err)
+	}
+	challenge := sha3.Sum256(bin)
+	return challenge[:]
+}
+
+// Runs a round of the protocol
+func (c *Client) round() {
+	challenge := c.GenerateChallenge()
+	prf := c.Mine(challenge)
+
+	send := true
+
+	for {
+		select {
+		case b := <-c.sols:
+			// probably can't trust the others in final version..
+			if b.Hash.Proof.Quality > prf.Quality {
+				send = false
+				break
+			}
+		default:
+			break
+		}
+	}
+	if send {
+		old, err := c.chain.Read(c.chain.LastBlock)
+		if err != nil {
+			panic(err)
+		}
+		// TODO: where do transactions come from??
+		b := block.NewBlock(old, *prf, nil, c.sk)
+		for _, r := range c.clients {
+			err := r.Call("Client.SendBlock", b, nil)
+			if err != nil {
+				panic(err)
+			}
+		}
+	}
+}
 
 func main() {
 
